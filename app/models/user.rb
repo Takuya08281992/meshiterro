38344308e37f0294fa5174156b661ae125051741
山崎拓也 <yamazakitakuya@yamazakitakuyanoMacBook-Pro.local>
2020-05-16 15:40:17 +0900

    [Finish]5-4

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :post_image, dependent: :destroy
-  has_many :post_comment, dependent: :destoroy
+  has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 end
